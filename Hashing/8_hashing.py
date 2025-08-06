ea5a0cf2a56953b71ad982c2cd7fde888a3ff17f
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-08-06 05:36:43 +0300

    fixing the errors in hashing

--- Hashing/8_hashing.py
+++ Hashing/8_hashing.py
@@ -1,19 +1,16 @@
-n = [5,4,2,2,1,1,1,1,3,3,3,3,3,3,3,3] #prestore
+# n = [5,4,2,2,1,1,1,1,3,3,3,3,3,3,3,3] #prestore
 
-m = [5, 6, 7, 7, 1, 9, 111, 1, 1, 5, 1, 1]
-nums = n
+# m = [5, 6, 7, 7, 1, 9, 111, 1, 1, 5, 1, 1]
+
 # Brute force method
-def store_frequency_brute_force(nums):
+def store_frequency_brute_force(nums, m):
     
     for num in m:
         count = 0
-        for i in n:
+        for i in nums:
             if i == num:
                 count += 1
-
         print("Frequency of", num, "is", count)
-
-store_frequency_brute_force(m)
 
 # Time complexity: O(n*m) where n is the length of n and m is the length of m
 # In worst case, we have to iterate through the entire list n for each element in m.
@@ -21,7 +18,7 @@ store_frequency_brute_force(m)
 
 # Using hashlist
 
-def store_frequency_hashlist(nums):
+def store_frequency_hashlist(nums, m):
     
     hash_list =  [0] * (max(nums) + 1)  # Create a hash list with size max(nums) + 1
     for i in nums:
@@ -36,7 +33,7 @@ def store_frequency_hashlist(nums):
 # We iterate through nums once to build the hash list and then through m to get frequencies.
 # Space complexity: O(max(nums)) since we are using a hash list of size max(nums) + 1.
 
-def store_frequency_dictionary(nums):
+def store_frequency_dictionary(nums, m):
     hash_dict = {}
     for i in nums:
         hash_dict[i] = hash_dict.get(i, 0) + 1
@@ -45,7 +42,7 @@ def store_frequency_dictionary(nums):
             print("Frequency of", num, "is 0")
         else:
             print("Frequency of", num, "is", hash_dict.get(num, 0))
-      
+
 # Time complexity: O(n + m) where n is the length of nums and m is the length of m
 # We iterate through nums once to build the hash dictionary and then through m to get frequencies.
 # Space complexity: O(n) where n is the number of unique elements in nums,
@@ -59,6 +56,9 @@ def store_frequency_string(s, q):
         frequency_map[char] = frequency_map.get(char, 0) + 1
     for char in q:
         print(f"Frequency of '{char}' is {frequency_map.get(char, 0)}")
+
+
+
 
 
 
