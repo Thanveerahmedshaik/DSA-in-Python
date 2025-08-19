d153347d6ff415692ae025a62d2d469b9ce8cc3d
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-08-19 05:50:38 +0300

    feat(sorting): add bubble sort implementation with tests

---
+++ Sorting/test_bubble_sort.py
@@ -0,0 +1,19 @@
+from bubble_sort import Solution
+
+def test_empty_list():
+  assert Solution().bubble_sort([]) == []
+
+def test_single_element():
+  assert Solution().bubble_sort([5]) == [5]
+
+def test_sorted_list():
+  assert Solution().bubble_sort([1, 2, 3]) == [1, 2, 3]
+
+def test_reverse_list():
+  assert Solution().bubble_sort([3, 2, 1]) == [1, 2, 3]
+
+def test_with_duplicates():
+  assert Solution().bubble_sort([4, 1, 4, 2]) == [1, 2, 4, 4]
+
+def test_negative_numbers():
+  assert Solution().bubble_sort([-1, -3, 2, 0]) == [-3, -1, 0, 2]
