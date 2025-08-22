637842c090861fc4400d531b0b2dd112d7f3158c
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-08-22 18:47:18 +0300

    Refactor: Code corrections and indentation problems

--- Sorting/mergesort.py
+++ Sorting/mergesort.py
@@ -1,30 +1,27 @@
-Class Solution:
-  def merge_sort(self, arr: List[int]) -> List[int]:
-    if len(arr) > 1:
-      mid = len(arr) // 2
-      left_half = arr[:mid]
-      right_half = arr[mid:]
+class Solution:
+    def mergeSort(self, nums):
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums) // 2
+        left_half = self.mergeSort(nums[:mid])
+        right_half = self.mergeSort(nums[mid:])
+        
+        return self.merge(left_half, right_half)
 
-      left_sorted = self.merge_sort(left_half)
-      right_sorted = self.merge_sort(right_half)
-
-      return self.merge(left_sorted, right_sorted)
-    return arr
-
-  def merge(self, left: List[int], right: List[int]) -> List[int]:
-    merged = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-      if left[i] < right[j]:
-        merged.append(left[i])
-        i += 1
-      else:
-        merged.append(right[j])
-        j += 1
-    merged.extend(left[i:])
-    merged.extend(right[j:])
-    return merged
-
-
-# The time complexity of merge sort is O(N log N) for all cases.
-# The space complexity of merge sort is O(N) because it requires additional space for the temporary arrays.
+    def merge(self, left, right):
+        sorted_array = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                sorted_array.append(left[i])
+                i += 1
+            else:
+                sorted_array.append(right[j])
+                j += 1
+        
+        sorted_array.extend(left[i:])
+        sorted_array.extend(right[j:])
+        
+        return sorted_array
