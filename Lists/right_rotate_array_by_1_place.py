e688c22de7d0be28cc881c5cd1cf84c5ac4fcbc0
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-08-25 18:16:45 +0300

    feat(array): add rotate array with slicing (O(n) space) and in-place shift(O(1) space)

---
+++ Lists/right_rotate_array_by_1_place.py
@@ -0,0 +1,29 @@
+# Function to rotate the array by one place using slicing method
+
+def rotate_array_by_one(arr: list[int]) -> list[int]:
+    arr[:] = [arr[-1]] + arr[:-1]
+    return arr
+# Time complexity using slicing is O(n) because we iterate through the entire array.
+# Space complexity is O(n) because we because slicing and concatenation makes a new list.
+
+
+# Function to rotate the array by one place using the traditional method
+def rotate_array_by_one(arr: list[int]) -> list[int]:
+    if not arr:
+        return arr  # Handle empty array
+
+    last_element = arr[-1]  # Store the last element
+    n = len(arr)
+
+    # Shift elements to the right
+    for i in range(n - 2, -1, -1):
+        arr[i + 1] = arr[i]
+
+    arr[0] = last_element  # Place the last element at the front
+    return arr
+
+
+# Time complexity using the traditional method is O(n) because we iterate throught 
+# the entire list 
+# Space complexity is O(1) because we modify the array in place
+
